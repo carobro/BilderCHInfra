b9434b7e3d2f268df10a0e7acac22ce79370c589
carobro <carolin.bronowicz@fhnw.ch>
2025-03-25 15:19:36 +0100

    Test

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
 import * as func from "./functions.js";
 
 Promise.all([
-    d3.json("../data/links.json"),
-    d3.json("../data/switzerland.geojson"),
-    d3.json("../data/sichtbarkeit.geojson"),
-    fetch("data/nodes.json")  // statt "/data/input.json"
-  .then(response => response.json())
-  .then(data => console.log(data))
-
-
+    d3.json("../BilderCHInfra/data/nodes.json"),
+    d3.json("../BilderCHInfra/data/links.json"),
+    d3.json("../BilderCHInfra/data/switzerland.geojson"),
+    d3.json("../BilderCHInfra/data/sichtbarkeit.geojson"),
 ]).then(([nodes, links, switzerland, sichtbarkeit]) => {
     const svg = d3.select("svg");
     const width = window.innerWidth * 0.7;
